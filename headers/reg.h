54282490276db025f47fec58f5d4296b807b2255
liu wenbo <1711042219@qq.com>
2020-06-09 00:29:31 +0800

    fixed some bugs

--- headers/reg.h
+++ headers/reg.h
@@ -1,8 +1,12 @@
-//
-// Created by os on 2020/6/9.
-//
-
 #ifndef CO_REG_H
 #define CO_REG_H
 
+#include "global.h"
+
+enum regs{
+	EAX,EBX,ECX,EDX,MEM,
+};
+extern std::string regs_convert_table[5];
+
+regs where_is_the_var(const std::string &var);
 #endif //CO_REG_H
---
+++ headers/reg_func_def.h
@@ -0,0 +1,4 @@
+#ifndef CO_REG_FUNC_DEF_H
+#define CO_REG_FUNC_DEF_H
+
+#endif //CO_REG_FUNC_DEF_H
